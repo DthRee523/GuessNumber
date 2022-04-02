5053f87864aff1e4771de3e15e602d2ed56c4e19
DthRee523 <l928787694@gmail.com>
2022-04-02 21:00:25 +0800

    add project

---
+++ build-GuessNumber-Desktop_Qt_5_12_12_GCC_64bit-Debug/CMakeFiles/GuessNumber.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GuessNumber.dir/main.cpp.o"
+  "CMakeFiles/GuessNumber.dir/main.cpp.o.d"
+  "GuessNumber"
+  "GuessNumber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GuessNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
